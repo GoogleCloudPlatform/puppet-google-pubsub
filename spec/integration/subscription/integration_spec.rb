662ca9e70b22896fcc36e8bae713c63d20dd4443
Nathan McKinley <nmckinley@google.com>
2018-09-07 01:24:29 +0000

    Add VCR-based integration / unit tests.

--- spec/integration/subscription/integration_spec.rb
+++ spec/integration/subscription/integration_spec.rb
@@ -24,25 +24,36 @@
 #     CONTRIBUTING.md located at the root of this package.
 #
 # ----------------------------------------------------------------------------
+require 'spec_helper'
+require 'vcr'
 
-source 'https://rubygems.org'
-group :test do
-  gem 'google-api-client'
-  gem 'googleauth'
-  gem 'metadata-json-lint'
-  gem 'parallel_tests'
-  gem 'puppet', ENV['PUPPET_GEM_VERSION'] || '>= 4.2.0'
-  gem 'puppet-lint'
-  gem 'puppet-lint-unquoted_string-check'
-  gem 'puppet-syntax'
-  gem 'puppetlabs_spec_helper'
-  gem 'rake', '~> 10.0'
-  gem 'rspec'
-  gem 'rspec-mocks'
-  gem 'rspec-puppet'
-  gem 'rubocop'
-  gem 'semantic_puppet'
-  gem 'simplecov'
-  gem 'vcr'
-  gem 'webmock'
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
+describe 'subscription.create', vcr: true do
+  it 'creates and destroys non-existent subscription' do
+    puts 'pre-destroying subscription'
+    VCR.use_cassette('pre_destroy_subscription') do
+      run_example('delete_subscription')
+    end
+    puts 'creating subscription'
+    VCR.use_cassette('create_subscription') do
+      run_example('subscription')
+    end
+    puts 'checking that subscription is created'
+    VCR.use_cassette('check_subscription') do
+      validate_no_flush_calls('subscription')
+    end
+    puts 'destroying subscription'
+    VCR.use_cassette('destroy_subscription') do
+      run_example('delete_subscription')
+    end
+    puts 'confirming subscription destroyed'
+    VCR.use_cassette('check_destroy_subscription') do
+      validate_no_flush_calls('delete_subscription')
+    end
+  end
 end
